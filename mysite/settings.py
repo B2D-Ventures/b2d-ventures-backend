d76dfe4ec8b052f149076058acb27071fa020acb
Krittin SETDHAVANICH <krittin.set@ku.th>
2024-11-07 21:34:24 +0700

    fix: remove dup ALLOWED HOST

--- mysite/settings.py
+++ mysite/settings.py
@@ -180,13 +180,6 @@ SPECTACULAR_SETTINGS = {
     "VERSION": "1.0.0",
     "SERVE_PERMISSIONS": ["rest_framework.permissions.IsAdminUser"],
 }
-ALLOWED_HOSTS = [
-  "localhost",
-  "127.0.0.1",
-  ".onrender.com",
-    'b2d-ventures-backend.onrender.com',
-    '*'
-]
 INTERNAL_IPS = ["127.0.0.1", "10.0.2.2"]
 
 # Google OAuth Configuration
